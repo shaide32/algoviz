291b3d008ba07bb88e041c0cb68906400828d1aa
Shahrukh Haider <shahrukh.haider@servicenow.com>
2019-08-24 20:55:36 +0530

    basic structure is complete

---
+++ src/components/Sidebar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+const LinkItem = ({})
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            opened: true
+        }
+    }
+    render() {
+        return(
+            <div className="sidebar">
+                <span>Company Logo</span>
+                <h2>BubbleSort</h2>
+            </div>
+        );
+    }
+}
+
+export default Sidebar;
